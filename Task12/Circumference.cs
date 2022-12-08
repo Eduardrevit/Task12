7692b3f7af8ebd0003cb197dcb20a0e81671a81e
Эдуард <@LAPTOP-2C6APU5G>
2022-12-08 10:16:42 +0300

    Добавьте файлы проекта.

---
+++ Task12/Circumference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    static class Circumference
+    {
+        static public double GetLenth(double r)
+        {
+            return 2 * r * Math.PI;
+        }
+        static public double GetSquare(double r)
+        {
+            return r * r * Math.PI;
+        }
+        static public string GetCheck(double r, double x0, double y0, double x, double y)
+        {
+            double a = Math.Sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0));
+            if (a<=r)
+                return "Точка принадлежит окружности";
+            else
+                return "Точка НЕ принадлежит окружности";
+        }
+
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности r=");
+            double r = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Введите координату центра окружности x0=");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату центра окружности y0=");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите координату проверяемой точки x=");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координаты проверяемой точки y=");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double square = Circumference.GetSquare(r);
+            string point = Circumference.GetCheck(r, x0, y0, x, y);
+
+            Console.WriteLine($"Площадь круга = {square:.00}. {point}");
+            Console.ReadKey();
+        }
+    }
+}
